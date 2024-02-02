070f89a64d34d9bb41d9be074602e85151315e93
dblloyd <d.brandon.lloyd@gmail.com>
2024-02-01 21:59:25 -0700

    Added .cpp file

---
+++ shims.d.ts
@@ -0,0 +1,11 @@
+// Auto-generated. Do not edit.
+declare namespace test_ext {
+
+    /**
+     * the func
+     */
+    //% shim=test_ext::_thefunc
+    function _thefunc(): int32;
+}
+
+// Auto-generated. Do not edit. Really.
---
+++ test_ext.cpp
@@ -0,0 +1,15 @@
+#include "pxt.h"
+
+namespace test_ext {
+
+/**
+ * the func
+ */
+//%
+int _thefunc()
+{
+    return 43;
+}
+
+
+} // namespace test_ext
